32d69883838f64d6fb83c92094e6bff79458dbf5
Tim Keresey <tim.keresey@gmail.com>
2020-11-10 09:51:40 -0700

    write fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,11 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (longUrl, title) => {
+  return fetch ('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: {long_url: longUrl, title: title}
+  })
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,6 +15,7 @@ export class App extends Component {
   componentDidMount() {
     getUrls()
     .then(data => this.setState({ urls: data.urls }))
+    .catch(error = this.setState({error: error.message}))
   }
 
   render() {
